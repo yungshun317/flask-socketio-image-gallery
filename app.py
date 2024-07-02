8eca12c1dfd21170c80d82ec71f0fab931061a78
yungshun317 <seraph317@gmail.com>
2024-07-03 05:25:27 +0800

    [FEAT] Add Flask-SocketIO to the Flask application

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'
 app.config['DEBUG'] = True
 socketio = SocketIO(app)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    socketio.run(app)
